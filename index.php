bd5013d602d670a5557c3e54e22c250a5c5cabc0
Jakub Korál <jakub.koral@apiary.io>
2013-02-16 11:44:27 +0100

    adminer

--- index.php
+++ index.php
@@ -1,15 +1,38 @@
 <?php
 # This function reads your DATABASE_URL configuration automatically set by Heroku
 # the return value is a string that will work with pg_connect
-function pg_connection_string() {
-  // we will fill this out next
+if (isset($_SERVER["CLEARDB_DATABASE_URL"])) {
+  $db = parse_url($_SERVER["CLEARDB_DATABASE_URL"]);
+  define("DB_NAME", trim($db["path"],"/"));
+  define("DB_USER", $db["user"]);
+  define("DB_PASSWORD", $db["pass"]);
+  define("DB_HOST", $db["host"]);
 }
-# Establish db connection
-$db = mysql_connect(mysql_connection_string());
-if (!$db) {
-   echo "Database connection error."
-   exit;
+else {
+  die("Your heroku CLEARDB_DATABASE_URL does not appear to be correctly specified.");
 }
-$result = mysql_query($db, "SELECT statement goes here");
 
 
+function adminer_object() {
+
+  class AdminerSoftware extends Adminer {
+
+  function name() {
+    // custom name in title and heading
+    return 'Strepiny';
+  }
+
+  function credentials() {
+    // server, username and password for connecting to database
+    return array(DB_HOST, DB_USER, DB_PASSWORD);
+  }
+
+  function login($login, $password) {
+    // validate user submitted credentials
+    return ($login == 'root' && $password == '');
+  }
+
+  return new AdminerSoftware;
+}
+
+include "./adminer.php";
