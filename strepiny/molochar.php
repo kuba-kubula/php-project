a9318d66dc507841f7eeabc4e0d28ed0874cf520
Jakub Korál <jakub.koral@apiary.io>
2013-03-02 13:03:01 +0100

    Engineers status window about the core status & instability

---
+++ strepiny/molochar.php
@@ -0,0 +1,26 @@
+<?php
+require_once("_settings/settings.php");
+include("svg/hexagon.php");
+
+$nestabilita = $db->fetch1Assoc('SELECT * FROM strepiny_nestabilita WHERE id="1";');
+
+?><!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+<?php echo (isset($_GET["robot"]) && $_GET['robot'] == 'true') ? '<meta http-equiv="refresh" content="'.$robotTime.'">' : ''; ?>
+  <link rel="stylesheet" href="strepiny.css" />
+  <title>Nestabilita jadra</title>
+</head>
+
+<body bgcolor="#0b0d0d" style="border: 0; margin: 0; padding: 0; overflow:hidden; font-family:Omikron, omikron-webfont; font-size:100px; color:white;">
+<center>
+celkova nestabilita<br />
+<?php echo $nestabilita['nc']; ?><br/>
+odber energie<br />
+<?php echo 100+$nestabilita['dnc']; ?>%
+</center>
+
+   </body>
+</head>
+</html>
