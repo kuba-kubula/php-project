71b216e197f83b2c5fedb6433c1b98d2e260c6ca
Jakub Korál <jakub.koral@apiary.io>
2013-03-02 18:59:42 +0100

    Adminer works locally and on heroku too

--- index.php
+++ index.php
@@ -6,27 +6,23 @@ if (isset($_SERVER["CLEARDB_DATABASE_URL"])) {
   define("CLEAR_DB_USER", $db["user"]);
   define("CLEAR_DB_PASSWORD", $db["pass"]);
   define("CLEAR_DB_HOST", $db["host"]);
-}
-else {
-  die("Your heroku CLEARDB_DATABASE_URL does not appear to be correctly specified.");
-}
 
-
-function adminer_object() {
-  class AdminerSoftware extends Adminer {
-    function credentials() {
-      // server, username and password for connecting to database
-      return array(CLEAR_DB_HOST, CLEAR_DB_USER, CLEAR_DB_PASSWORD);
+  function adminer_object() {
+    class AdminerSoftware extends Adminer {
+      function credentials() {
+        // server, username and password for connecting to database
+        return array(CLEAR_DB_HOST, CLEAR_DB_USER, CLEAR_DB_PASSWORD);
+      }
+      function database() {
+        return CLEAR_DB_NAME;
+      }
+      function login($login, $password) {
+        // validate user submitted credentials
+        return ($login == $_SERVER['LOGIN_USER'] || $login == $_SERVER['LOGIN_USER2']);
+      }
     }
-    function database() {
-      return CLEAR_DB_NAME;
-    }
-    function login($login, $password) {
-      // validate user submitted credentials
-      return ($login == $_SERVER['LOGIN_USER'] || $login == $_SERVER['LOGIN_USER2']);
-    }
+    return new AdminerSoftware;
   }
-  return new AdminerSoftware;
 }
 
 include "./adminer.php";
